06413d136a4f6d45b09dac8e2ff88b5e5985be08
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-06-05 16:28:34 +0200

    validation et test fait

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
 
   test "should get index" do
@@ -17,7 +18,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+      post products_url, params: { product: { title: @title, description: @product.description, image_url: @product.image_url, price: @product.price } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +35,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { title: @product. title, description: @product.description, image_url: @product.image_url, price: @product.price } }
+    patch product_url(@product), params: { product: { title: @title, description: @product.description, image_url: @product.image_url, price: @product.price } }
     assert_redirected_to product_url(@product)
   end
 
