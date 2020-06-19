bee0c062e151dbd66c252b10fe87ecc0e237e4e1
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-06-19 16:54:02 +0200

    mail, model user create

--- config/environment.rb
+++ config/environment.rb
@@ -11,3 +11,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :test
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
 #---
 Rails.application.routes.draw do
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see
   # https://guides.rubyonrails.org/routing.html
 end
